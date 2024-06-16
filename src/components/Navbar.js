14064198e60a95a697cd1ecb266a0a6103fce0a1
AJAY-RAGHAVA <118554390+AJAY-RAGHAVA@users.noreply.github.com>
2024-06-16 20:53:52 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Import the CSS file
 
 function Navbar() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const handleToggle = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <h1>DONTHU SAI NAGA VENKATA AJAY RAGHAVA</h1><br />
         <h3>Software Engineer</h3>
       </div>
-      <div className="navbar-right">
+      <button id="navbar-toggle" className="navbar-toggle" onClick={handleToggle}>
+        ☰
+      </button>
+      <div className={`navbar-right ${menuActive ? 'active' : ''}`}>
         <ul className="navbar-menu">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/skills">Skills</Link></li>
-          <li><Link to="/resume">Resume</Link></li>
-          <li><Link to="/portfolio">Projects</Link></li>
-          <li><Link to="/achievements">Achievements</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/" onClick={() => setMenuActive(false)}>Home</Link></li>
+          <li><Link to="/skills" onClick={() => setMenuActive(false)}>Skills</Link></li>
+          <li><Link to="/resume" onClick={() => setMenuActive(false)}>Resume</Link></li>
+          <li><Link to="/portfolio" onClick={() => setMenuActive(false)}>Projects</Link></li>
+          <li><Link to="/achievements" onClick={() => setMenuActive(false)}>Achievements</Link></li>
+          <li><Link to="/contact" onClick={() => setMenuActive(false)}>Contact</Link></li>
         </ul>
       </div>
     </nav>
